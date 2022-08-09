d1f3798715973288cdfdcdddeb18f99fa1ea3b66
shjung53 <gcjung777@gmail.com>
2022-08-10 00:06:49 +0900

    xml dimen arrange

--- app/src/main/java/com/example/tripper/ui/main/home/HomeVPAdapter.kt
+++ app/src/main/java/com/example/tripper/ui/main/home/HomeVPAdapter.kt
@@ -3,6 +3,7 @@ package com.example.tripper.ui.main.home
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
+//홈 탭레이아웃 어댑터
 class HomeVPAdapter(fragment: Fragment): FragmentStateAdapter(fragment) {
 
     override fun getItemCount(): Int = 3
